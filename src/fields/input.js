7916db5b258cf907fb26c2293e72b068a1df9aab
Abel van Hoek <abel@eitje.app>
2024-03-19 19:32:21 +0100

    prettier + dont show char counter in case of new form

--- src/fields/input.js
+++ src/fields/input.js
@@ -1,26 +1,19 @@
-import React, { Component, Fragment, useRef } from "react";
-import { Input as AntInput } from "antd";
-import moment from "moment";
-import {
-  useFormField,
-  usePicker,
-  makeField,
-  makeLegacyField,
-  makeRegisteredField,
-  t,
-} from "@eitje/form";
-import utils from "@eitje/utils";
+import React, {Component, Fragment, useRef} from 'react'
+import {Input as AntInput} from 'antd'
+import moment from 'moment'
+import {useFormField, usePicker, makeField, makeLegacyField, makeRegisteredField, t} from '@eitje/form'
+import utils from '@eitje/utils'
 
 const change = (props, val, event) => {
-  const { formatValue, onChange } = props;
-  let newVal = val;
+  const {formatValue, onChange} = props
+  let newVal = val
   if (formatValue) {
-    newVal = formatValue(val);
-    if (_.isNaN(newVal)) return;
+    newVal = formatValue(val)
+    if (_.isNaN(newVal)) return
   }
 
-  onChange(newVal, event);
-};
+  onChange(newVal, event)
+}
 
 const Input = (props) => {
   const {
@@ -30,27 +23,24 @@ const Input = (props) => {
     suffix,
     charCounterProps = {},
     innerRef,
+    newForm,
     hideCharCounter,
     maxLength,
-    autocomplete = "nope",
-    autoSize = { minRows: 2, maxRows: 12 },
+    autocomplete = 'nope',
+    autoSize = {minRows: 2, maxRows: 12},
     disableAutoComplete,
     readOnly,
     ...rest
-  } = props;
-  const InputEl = textarea
-    ? AntInput.TextArea
-    : secure
-    ? AntInput.Password
-    : AntInput;
-  const _suffix = utils.funcOrVal(suffix, props);
+  } = props
+  const InputEl = textarea ? AntInput.TextArea : secure ? AntInput.Password : AntInput
+  const _suffix = utils.funcOrVal(suffix, props)
 
   const handleFocus = (e) => {
     if (disableAutoComplete) {
-      e.target.removeAttribute("readonly");
+      e.target.removeAttribute('readonly')
     }
-    props.onFocus?.(e);
-  };
+    props.onFocus?.(e)
+  }
   return (
     <Fragment>
       <InputEl
@@ -66,30 +56,21 @@ const Input = (props) => {
         onFocus={handleFocus}
         readOnly={disableAutoComplete || readOnly}
       />
-      {!!maxLength && !hideCharCounter && (
-        <CharCounter
-          value={value}
-          maxLength={maxLength}
-          {...charCounterProps}
-        />
-      )}
+      {!newForm && !!maxLength && !hideCharCounter && <CharCounter value={value} maxLength={maxLength} {...charCounterProps} />}
     </Fragment>
-  );
-};
+  )
+}
 
-const makeCharClass = (
-  charsLeft,
-  { warningThreshold = 10, dangerThreshold = 5 }
-) => {
-  if (charsLeft < dangerThreshold) return "char-counter-danger";
-  if (charsLeft < warningThreshold) return "char-counter-warning";
-  return "";
-};
+const makeCharClass = (charsLeft, {warningThreshold = 10, dangerThreshold = 5}) => {
+  if (charsLeft < dangerThreshold) return 'char-counter-danger'
+  if (charsLeft < warningThreshold) return 'char-counter-warning'
+  return ''
+}
 
-const CharCounter = ({ maxLength, value = "", ...rest }) => {
-  const charsLeft = maxLength - (value?.length || 0);
-  const className = makeCharClass(charsLeft, rest);
-  return <p className={`char-counter ${className}`}>{charsLeft}</p>;
-};
+const CharCounter = ({maxLength, value = '', ...rest}) => {
+  const charsLeft = maxLength - (value?.length || 0)
+  const className = makeCharClass(charsLeft, rest)
+  return <p className={`char-counter ${className}`}>{charsLeft}</p>
+}
 
-export default Input;
+export default Input
